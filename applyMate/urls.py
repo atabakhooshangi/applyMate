15758f4a659b7b135e76cd6abbf1c34192bd22b3
atabak hooshangi <atabak.hooshangi.1990@gmail.com>
2024-06-14 15:26:24 +0200

    fix config for vercel and adjust urls

--- applyMate/urls.py
+++ applyMate/urls.py
@@ -21,7 +21,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('applications/', include('core.urls')),
+    path('', include('core.urls')),
 
 ]
 
