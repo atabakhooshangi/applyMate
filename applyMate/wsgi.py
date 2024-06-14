95025b5cae26c7826f3bfe8695d6dea558f082e3
atabak hooshangi <atabak.hooshangi.1990@gmail.com>
2024-06-14 15:20:07 +0200

    added vercel config

--- applyMate/wsgi.py
+++ applyMate/wsgi.py
@@ -14,3 +14,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'applyMate.settings')
 
 application = get_wsgi_application()
+
+vercel_app = application
