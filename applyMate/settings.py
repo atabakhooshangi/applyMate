2ac9c27a2b2dd58f833ed6128f4e234f3f8e3cec
atabak hooshangi <atabak.hooshangi.1990@gmail.com>
2024-06-14 16:33:35 +0200

    removed static config from vercel.json

--- applyMate/settings.py
+++ applyMate/settings.py
@@ -87,7 +87,6 @@ DATABASES = {
         'PORT': os.environ.get('PG_PORT'),
     }
 }
-print(DATABASES)
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
